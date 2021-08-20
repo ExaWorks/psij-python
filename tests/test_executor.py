70bf9909c7df1a5892be53ed55fb54c825dc2781
Mike <mhategan@gmail.com>
2021-08-20 12:03:58 -0700

    Basic code to instantiate custom configs based on executor.
    This is mostly there to make it easy to edit things for debugging
    purposes.

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-from psij import SubmitException, Job, JobExecutor, JobSpec, JobState, JobStatus
+from psij import SubmitException, Job, JobExecutor, JobSpec, JobState, JobStatus, JobExecutorConfig
+from psij.executors.slurm import SlurmExecutorConfig
 
 
 def assert_completed(status: JobStatus) -> None:
@@ -13,9 +14,16 @@ def assert_completed(status: JobStatus) -> None:
         raise RuntimeError('Unexpected job state: %s' % status.state)
 
 
+def get_config(executor: str) -> JobExecutorConfig:
+    if executor == 'slurm':
+        return SlurmExecutorConfig()
+    else:
+        return None
+
+
 def test_simple_job(executor: str) -> None:
     job = Job(JobSpec(executable='/bin/date'))
-    exec = JobExecutor.get_instance(executor)
+    exec = JobExecutor.get_instance(executor, config=get_config(executor))
     exec.submit(job)
     status = job.wait()
     assert_completed(status)
@@ -25,7 +33,7 @@ def test_simple_job_redirect(executor: str) -> None:
     with TemporaryDirectory() as td:
         outp = Path(td, 'stdout.txt')
         job = Job(JobSpec(executable='/bin/echo', arguments=['-n', '_x_'], stdout_path=outp))
-        exec = JobExecutor.get_instance(executor)
+        exec = JobExecutor.get_instance(executor, config=get_config(executor))
         exec.submit(job)
         status = job.wait()
         assert_completed(status)
@@ -36,7 +44,7 @@ def test_simple_job_redirect(executor: str) -> None:
 
 def test_attach(executor: str) -> None:
     job = Job(JobSpec(executable='/bin/sleep', arguments=['1']))
-    exec = JobExecutor.get_instance(executor)
+    exec = JobExecutor.get_instance(executor, config=get_config(executor))
     exec.submit(job)
     job.wait(target_states=[JobState.ACTIVE])
     native_id = job.native_id
@@ -50,7 +58,7 @@ def test_attach(executor: str) -> None:
 
 def test_cancel(executor: str) -> None:
     job = Job(JobSpec(executable='/bin/sleep', arguments=['1']))
-    exec = JobExecutor.get_instance(executor)
+    exec = JobExecutor.get_instance(executor, config=get_config(executor))
     exec.submit(job)
     job.wait(target_states=[JobState.ACTIVE])
     job.cancel()
@@ -61,7 +69,7 @@ def test_cancel(executor: str) -> None:
 
 def test_failing_job(executor: str) -> None:
     job = Job(JobSpec(executable='/bin/false'))
-    exec = JobExecutor.get_instance(executor)
+    exec = JobExecutor.get_instance(executor, config=get_config(executor))
     exec.submit(job)
     status = job.wait()
     assert status is not None
@@ -72,7 +80,7 @@ def test_failing_job(executor: str) -> None:
 
 def test_missing_executable(executor: str) -> None:
     job = Job(JobSpec(executable='/bin/no_such_file_or_directory'))
-    exec = JobExecutor.get_instance(executor)
+    exec = JobExecutor.get_instance(executor, config=get_config(executor))
     # we don't know if this will fail with an exception or JobState.FAILED, so handle both
     try:
         exec.submit(job)
