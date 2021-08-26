8ae42357b234f707d70564e267bd8588676c28ee
Andre Merzky <andre@merzky.net>
2021-08-26 12:17:02 +0200

    parameterize tests to run across multiple executor backends

--- tests/test_local_jobs.py
+++ tests/test_local_jobs.py
@@ -1,21 +1,28 @@
+import pytest
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from psij import SubmitException, Job, JobExecutor, JobSpec, JobState
 
+tests = [['local', None],
+         ['saga', 'fork://localhost/'],
+         ['saga', 'ssh://localhost/']]
 
-def test_simple_job() -> None:
+
+@pytest.mark.parametrize('name,url', tests)
+def test_simple_job(name, url) -> None:
     job = Job(JobSpec(executable='/bin/date'))
-    exec = JobExecutor.get_instance('local')
+    exec = JobExecutor.get_instance(name=name, url=url)
     exec.submit(job)
     job.wait()
 
 
-def test_simple_job_redirect() -> None:
+@pytest.mark.parametrize('name,url', tests)
+def test_simple_job_redirect(name, url) -> None:
     with TemporaryDirectory() as td:
         outp = Path(td, 'stdout.txt')
         job = Job(JobSpec(executable='/bin/echo', arguments=['-n', '_x_'], stdout_path=outp))
-        exec = JobExecutor.get_instance('local')
+        exec = JobExecutor.get_instance(name=name, url=url)
         exec.submit(job)
         job.wait()
         f = outp.open("r")
@@ -23,9 +30,10 @@ def test_simple_job_redirect() -> None:
         assert contents == '_x_'
 
 
-def test_attach() -> None:
+@pytest.mark.parametrize('name,url', tests)
+def test_attach(name, url) -> None:
     job = Job(JobSpec(executable='/bin/sleep', arguments=['1']))
-    exec = JobExecutor.get_instance('local')
+    exec = JobExecutor.get_instance(name=name, url=url)
     exec.submit(job)
     job.wait(target_states=[JobState.ACTIVE])
     native_id = job.native_id
@@ -36,9 +44,10 @@ def test_attach() -> None:
     job2.wait()
 
 
-def test_cancel() -> None:
+@pytest.mark.parametrize('name,url', tests)
+def test_cancel(name, url) -> None:
     job = Job(JobSpec(executable='/bin/sleep', arguments=['1']))
-    exec = JobExecutor.get_instance('local')
+    exec = JobExecutor.get_instance(name=name, url=url)
     exec.submit(job)
     job.wait(target_states=[JobState.ACTIVE])
     job.cancel()
@@ -47,9 +56,10 @@ def test_cancel() -> None:
     assert status.state == JobState.CANCELED
 
 
-def test_failing_job() -> None:
+@pytest.mark.parametrize('name,url', tests)
+def test_failing_job(name, url) -> None:
     job = Job(JobSpec(executable='/bin/false'))
-    exec = JobExecutor.get_instance('local')
+    exec = JobExecutor.get_instance(name=name, url=url)
     exec.submit(job)
     status = job.wait()
     assert status is not None
@@ -58,9 +68,10 @@ def test_failing_job() -> None:
     assert status.exit_code != 0
 
 
-def test_missing_executable() -> None:
+@pytest.mark.parametrize('name,url', tests)
+def test_missing_executable(name, url) -> None:
     job = Job(JobSpec(executable='/bin/no_such_file_or_directory'))
-    exec = JobExecutor.get_instance('local')
+    exec = JobExecutor.get_instance(name=name, url=url)
     # we don't know if this will fail with an exception or JobState.FAILED, so handle both
     try:
         exec.submit(job)
