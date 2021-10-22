278585f7c85763f33bb7c63932cae61dfbf8782d
James Corbett <corbett8@llnl.gov>
2021-10-22 15:38:09 -0700

    Add Cobalt executor

--- src/psij/executors/__init__.py
+++ src/psij/executors/__init__.py
@@ -4,5 +4,12 @@ from .local import LocalJobExecutor
 from .rp import RPJobExecutor
 from .flux import FluxJobExecutor
 from .lsf import LsfJobExecutor
+from .cobalt import CobaltJobExecutor
 
-__all__ = ['LocalJobExecutor', 'RPJobExecutor', 'FluxJobExecutor', 'LsfJobExecutor']
+__all__ = [
+    'LocalJobExecutor',
+    'RPJobExecutor',
+    'FluxJobExecutor',
+    'LsfJobExecutor',
+    'CobaltJobExecutor'
+]
