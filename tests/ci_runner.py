71b9b49b96a390b2f8d7823d3961478d682c45e6
hategan <mhategan@gmail.com>
2022-01-07 22:13:30 -0800

    Add test plugin dirs to PYTHONPATH when running ci tests

--- tests/ci_runner.py
+++ tests/ci_runner.py
@@ -122,6 +122,8 @@ def run_branch_tests(conf: Dict[str, str], dir: Path, run_id: str, clone: bool =
     cwd = (dir / 'code') if clone else Path('.')
     env = dict(os.environ)
     env['PYTHONPATH'] = str(cwd.absolute() / 'src') + \
+        ':' + str(cwd.absolute() / 'tests' / 'plugins1') + \
+        ':' + str(cwd.absolute() / 'tests' / 'plugins2') + \
         (':' + env['PYTHONPATH'] if 'PYTHONPATH' in env else '')
     subprocess.run(args, cwd=cwd.absolute(), env=env)
 
