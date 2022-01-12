d9028490337aa3180ef0e650957cdfe57fdf081a
hategan <mhategan@gmail.com>
2022-01-11 20:51:27 -0800

    Allow some level of branch-specific customization in the way ci tests are launched.

--- tests/ci_runner.py
+++ tests/ci_runner.py
@@ -105,7 +105,8 @@ def run_branch_tests(conf: Dict[str, str], dir: Path, run_id: str, clone: bool =
                      site_id: Optional[str] = None, fake_branch_name: Optional[str] = None) -> None:
     # it would be nice if this could be run through make; however, gnu make cannot preserve
     # spaces or quotes in arguments
-    args = [sys.executable, '-m', 'pytest', '-v', '--upload-results', '--run-id', run_id]
+    args = ['tests/branch_test_wrapper.sh', sys.executable, '-m', 'pytest', '-v',
+            '--upload-results', '--run-id', run_id]
     if site_id:
         args.append('--id')
         args.append(site_id)
@@ -122,8 +123,6 @@ def run_branch_tests(conf: Dict[str, str], dir: Path, run_id: str, clone: bool =
     cwd = (dir / 'code') if clone else Path('.')
     env = dict(os.environ)
     env['PYTHONPATH'] = str(cwd.absolute() / 'src') + \
-        ':' + str(cwd.absolute() / 'tests' / 'plugins1') + \
-        ':' + str(cwd.absolute() / 'tests' / 'plugins2') + \
         (':' + env['PYTHONPATH'] if 'PYTHONPATH' in env else '')
     subprocess.run(args, cwd=cwd.absolute(), env=env)
 
