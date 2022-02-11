0c7a2775902e4123c2c0c3670097d83b804f06ed
Andreas Wilke <wilke@anl.gov>
2022-02-10 20:23:12 -0600

    Added helper function to convert path object to string

---
+++ src/psij/utils.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import sys
+
+def path_object_to_full_path( obj ) -> str :
+    p = None
+    if obj :
+        if type(obj).__name__ == "str" :
+            p = obj
+        elif type(obj).__name__ == "PosixPath" :
+            p = obj.as_posix()
+        else :
+            print(type(obj))
+            sys.exit("This type " + type(obj).__name__  + " for a path is not supported, use pathlib instead")
+    return p
