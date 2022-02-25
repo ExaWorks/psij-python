2860e525d3cb3a0d8c1856c4736502fcde666cec
Andreas Wilke <wilke@anl.gov>
2022-02-24 20:11:29 -0600

    Initial import export example script

---
+++ scripts/import-export.py
@@ -0,0 +1,37 @@
+import psij
+from psij.serialize import Export
+from psij.serialize import Import
+
+
+jex = psij.JobExecutor.get_instance('slurm')
+
+N=1 # number of jobs to run
+
+def make_job():
+    job = psij.Job()
+    spec = psij.JobSpec()
+    spec.executable = 'echo Hello World'
+    spec.arguments = ['10']
+    job.spec = spec
+    return job
+
+
+
+# Create Job and export
+e = Export()
+for i in range(N):
+    job = make_job()
+    e.export(obj=spec , dest="jobSpec." + str(i) + ".json")
+    
+# Import Job and submit
+i = Import()
+jobs = []    
+for i in range(N):    
+    job = psij.Job()
+    spec = i.load(src="jobSpec." + str(i) + ".json")
+    job.spec = spec
+    jobs.append(job)
+    jex.submit(job)
+
+for i in range(N):
+    jobs[i].wait()
