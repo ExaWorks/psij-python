91e98e1059c2e7d135c8460014ce2b12ba4716af
Ben Clifford <benc@hawaga.org.uk>
2022-02-28 12:03:14 +0000

    Use non-underscore descriptor names, following PR #152

--- src/psij-descriptors/pbspro_descriptor.py
+++ src/psij-descriptors/pbspro_descriptor.py
@@ -1,7 +1,7 @@
 from distutils.version import StrictVersion
 
-from psij._descriptor import _Descriptor
+from psij.descriptor import Descriptor
 
 
-__PSI_J_EXECUTORS__ = [_Descriptor(name='pbspro', version=StrictVersion('0.0.1'),
-                                   cls='psij.executors.batch.pbspro.PBSProJobExecutor')]
+__PSI_J_EXECUTORS__ = [Descriptor(name='pbspro', version=StrictVersion('0.0.1'),
+                                  cls='psij.executors.batch.pbspro.PBSProJobExecutor')]
