1fc62f2479595b289403dcb8110a50d84caacaa2
hategan <mhategan@gmail.com>
2022-06-21 11:27:52 -0700

    Print stdout/stderr when a test job unexpectedly fails.

--- tests/_test_tools.py
+++ tests/_test_tools.py
@@ -20,10 +20,22 @@ def _get_timeout(execparams: ExecutorTestParams) -> Optional[timedelta]:
         return None
 
 
-def assert_completed(status: Optional[JobStatus]) -> None:
+def _read_file(path: Optional[Path]) -> str:
+    if path is None:
+        return ''
+
+    with open(path, 'r') as f:
+        return f.read()
+
+
+def assert_completed(job: Job, status: Optional[JobStatus]) -> None:
     assert status is not None
     if status.state != JobState.COMPLETED:
-        raise AssertionError('Job not completed. Status message: %s' % status.message)
+        assert job.spec is not None
+        stdout = _read_file(job.spec.stdout_path)
+        stderr = _read_file(job.spec.stderr_path)
+        raise AssertionError('Job not completed. Status message: %s, stdout: %s, stderr: %s'
+                             % (status.message, stdout, stderr))
 
 
 def _get_executor_instance(ep: ExecutorTestParams, job: Job) -> JobExecutor:
