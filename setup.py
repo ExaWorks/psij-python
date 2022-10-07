061c58029f7428d111cf5b1009747ec5e0a767ef
Yadu Nand Babuji <yadudoc1729@gmail.com>
2022-10-07 15:00:41 -0500

    Adding license

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     setup(
         name='psi-j-python',
         version=VERSION,
-
+        license='MIT',
         description='''This is an implementation of the J/PSI (Portable Submission Interface for Jobs)
         specification.''',
 
