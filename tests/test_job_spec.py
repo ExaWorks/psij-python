01297a1e42a0d9e5d3a0e01d61b8f14fcd151ffb
Andre Merzky <andre@merzky.net>
2022-10-27 23:03:51 +0200

    make mypy happy

--- tests/test_job_spec.py
+++ tests/test_job_spec.py
@@ -6,20 +6,20 @@ from psij import JobSpec
 def test_environment_types() -> None:
 
     with pytest.raises(TypeError):
-        JobSpec(environment={'foo': 1})
+        JobSpec(environment={'foo': 1})                           # type: ignore
 
     with pytest.raises(TypeError):
-        JobSpec(environment={1: 'foo'})
+        JobSpec(environment={1: 'foo'})                           # type: ignore
 
     with pytest.raises(TypeError):
         spec = JobSpec()
-        spec.environment = {'foo': 1}
+        spec.environment = {'foo': 1}                             # type: ignore
 
     spec = JobSpec()
     assert spec.environment is None
 
     spec = JobSpec(environment={'foo': 'bar'})
-    assert spec.environment['foo'] == 'bar'
+    assert spec.environment['foo'] == 'bar'                       # type: ignore
 
     spec = JobSpec()
     spec.environment = {'foo': 'bar'}
@@ -27,5 +27,3 @@ def test_environment_types() -> None:
 
     spec.environment = {'foo': 'biz'}
     assert spec.environment['foo'] == 'biz'
-
-test_environment_types()
