40d1c8921e793062683b4f88e930cc244cb575da
hategan <mhategan@gmail.com>
2022-11-16 11:53:42 -0800

    Updated tests for spec checks to account for the fact that (most) checks happen on submit

--- tests/test_job_spec.py
+++ tests/test_job_spec.py
@@ -1,31 +1,32 @@
 
 import pytest
 
-from psij import JobSpec
+from psij import JobSpec, JobExecutor, Job
+
+
+def _test_spec(spec: JobSpec) -> None:
+    ex = JobExecutor.get_instance('local')
+    ex.submit(Job(spec))
 
 
 def test_environment_types() -> None:
+    with pytest.raises(TypeError):
+        _test_spec(JobSpec(environment={'foo': 1}))  # type: ignore
 
     with pytest.raises(TypeError):
-        JobSpec(environment={'foo': 1})                           # type: ignore
-
-    with pytest.raises(TypeError):
-        JobSpec(environment={1: 'foo'})                           # type: ignore
-
-    with pytest.raises(TypeError):
-        spec = JobSpec()
-        spec.environment = {'foo': 1}                             # type: ignore
+        _test_spec(JobSpec(environment={1: 'foo'}))  # type: ignore
 
     with pytest.raises(TypeError):
         spec = JobSpec()
         spec.environment = {'foo': 'bar'}
-        spec.environment['buz'] = 2                              # type: ignore
+        spec.environment['buz'] = 2  # type: ignore
+        _test_spec(spec)
 
     spec = JobSpec()
     assert spec.environment is None
 
     spec = JobSpec(environment={'foo': 'bar'})
-    assert spec.environment['foo'] == 'bar'                       # type: ignore
+    assert spec.environment['foo'] == 'bar'  # type: ignore
 
     spec = JobSpec()
     spec.environment = {'foo': 'bar'}
@@ -33,5 +34,6 @@ def test_environment_types() -> None:
 
     spec.environment = {'foo': 'biz'}
     assert spec.environment['foo'] == 'biz'
+
 
 test_environment_types()
