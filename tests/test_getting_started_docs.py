5a96b3f724621c47ecd3398845ae44ed56494357
Ramon Arambula <86633301+RamonAra209@users.noreply.github.com>
2023-01-04 16:49:47 -0800

    tests: Added doc tests with executor parameters

---
+++ tests/test_getting_started_docs.py
@@ -0,0 +1,78 @@
+"""Functions for testing the various code snippets available in psij docs - 'getting started'"""
+import os
+from time import sleep
+from psij import Job, JobExecutor, JobSpec, JobAttributes, JobStatus
+from executor_test_params import ExecutorTestParams
+from _test_tools import _get_executor_instance, _get_timeout, assert_completed, _make_test_dir
+
+status_callback_job_count = 3
+
+def test_getting_started_basic_usage(execparams: ExecutorTestParams) -> None:
+    print(f"\nGetting Started Basic Usage, env: {execparams.executor.__repr__()} \n")
+    job = Job(
+        JobSpec(
+            executable="/bin/date",
+            # attributes=JobAttributes(queue_name=QUEUE_NAME) #? Check if getting rid of the Queue name causes test to fail
+            launcher=execparams.launcher
+        )
+    )
+    ex = _get_executor_instance(execparams, job)
+    ex.submit(job)
+    status = job.wait(timeout=_get_timeout(execparams))
+    assert_completed(job, status)
+
+def test_getting_started_adding_complexity(execparams: ExecutorTestParams) -> None:
+    print(f"\nGetting Started Adding Complex, env: {execparams.executor.__repr__()} \n")
+    num_jobs = 3
+    for _ in range(num_jobs):
+        job = Job(
+            JobSpec(
+                executable="/bin/date",
+                # attributes=JobAttributes(queue_name=QUEUE_NAME)
+                launcher=execparams.launcher
+            )
+        )
+        ex = _get_executor_instance(execparams, job)
+        ex.submit(job)
+
+
+def test_getting_started_status_callbacks(execparams: ExecutorTestParams) -> None:
+    print(f"\nGetting Started Status Callbacks, env: {execparams.executor.__repr__()} \n")
+    def callback(job: Job, status: JobStatus) -> None:
+        global status_callback_job_count
+
+        if status.final:
+            print(f"Job {job} completed with status {status}")
+            status_callback_job_count -= 1
+
+    for _ in range(status_callback_job_count):
+        job = Job(JobSpec(executable='/bin/date', launcher=execparams.launcher))
+        ex = _get_executor_instance(execparams, job)
+        ex.submit(job)
+
+    while status_callback_job_count > 0:
+        sleep(0.01)
+
+
+def test_our_test(execparams: ExecutorTestParams) -> None:
+    print(f"\nGetting Started Dans Test, env: {execparams.executor.__repr__()} \n")
+    num_jobs = 3
+
+    def make_job() -> Job:
+        job = Job()
+        spec = JobSpec()
+        spec.attributes.queue_name = QUEUE_NAME
+        spec.executable = 'echo'
+        spec.arguments = ['HELLO WORLD!']
+        job.spec = spec
+        return job
+
+    jobs = []
+    for _ in range(num_jobs):
+        job: Job = make_job()
+        jobs.append(job)
+        jex = _get_executor_instance(execparams, job)
+        jex.submit(job)
+
+    for job in jobs:
+        job.wait()
