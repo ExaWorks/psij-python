d970527cdeb6347827af186dbab51389e0202ba4
hategan <mhategan@gmail.com>
2023-09-13 14:11:17 -0700

    Don't assume tests are run in the same directory as their source files.

--- tests/user_guide/test_attach.py
+++ tests/user_guide/test_attach.py
@@ -1,7 +1,11 @@
+import os
 import subprocess
 import sys
 
 
 def test_user_guide_attach() -> None:
-    p = subprocess.run([sys.executable, './submit.py'], check=True, capture_output=True)
-    subprocess.run([sys.executable, './attach.py'], input=p.stdout, check=True)
+    my_dir = os.dirname(os.realpath(__file__))
+    p = subprocess.run([sys.executable, os.path.join(my_dir, 'submit.py')], check=True,
+                       capture_output=True)
+    subprocess.run([sys.executable, os.path.join(my_dir, '/attach.py')], input=p.stdout,
+                   check=True)
