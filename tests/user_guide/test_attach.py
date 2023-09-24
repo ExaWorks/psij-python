a6053c554d527b3be2be935481b7547229f72643
hategan <mhategan@gmail.com>
2023-09-24 11:40:28 -0700

    Fixed test

--- tests/user_guide/test_attach.py
+++ tests/user_guide/test_attach.py
@@ -4,7 +4,7 @@ import sys
 
 
 def test_user_guide_attach() -> None:
-    my_dir = os.dirname(os.realpath(__file__))
+    my_dir = os.path.dirname(os.path.realpath(__file__))
     p = subprocess.run([sys.executable, os.path.join(my_dir, 'submit.py')], check=True,
                        capture_output=True)
     subprocess.run([sys.executable, os.path.join(my_dir, '/attach.py')], input=p.stdout,
