d3f9bfc32cb79cbeb68f776fea5ea001e2e4bfa2
hategan <mhategan@gmail.com>
2024-02-10 10:28:26 -0800

    Files used by jobs need to be on a shared FS. This is typically the case except that the ci_runner, when testing various branches, does the cloning in `/tmp`. One alternative solution would be to ensure this is moved to the home dir.

--- tests/test_nodefile.py
+++ tests/test_nodefile.py
@@ -4,7 +4,7 @@ from tempfile import TemporaryDirectory
 
 import pytest
 
-from _test_tools import assert_completed, _get_executor_instance
+from _test_tools import assert_completed, _get_executor_instance, _deploy
 from executor_test_params import ExecutorTestParams
 from psij import Job, JobSpec, ResourceSpecV1
 
@@ -14,17 +14,20 @@ NOT_TESTED = set(['rp', 'flux'])
 def test_nodefile(execparams: ExecutorTestParams) -> None:
     if execparams.executor in NOT_TESTED:
         pytest.skip('This test does not work with %s' % execparams.executor)
+    if execparams.launcher == 'single':
+        pytest.skip('This test does not work with the single launcher')
 
     my_path = os.path.dirname(os.path.realpath(__file__))
 
     N_PROC = 4
     with TemporaryDirectory(dir=Path.home() / '.psij' / 'test') as td:
         outp = Path(td, 'stdout.txt')
-        spec = JobSpec('/bin/bash', [os.path.join(my_path, 'test_nodefile.sh'), str(N_PROC)],
-                       stdout_path=outp)
-        job = Job(spec)
-        spec.resources = ResourceSpecV1(process_count=N_PROC)
-        ex = _get_executor_instance(execparams, job)
-        ex.submit(job)
-        status = job.wait()
-        assert_completed(job, status)
+        with _deploy(os.path.join(my_path, 'test_nodefile.sh')) as excp:
+            spec = JobSpec('/bin/bash', [str(excp), str(N_PROC)],
+                           stdout_path=outp)
+            job = Job(spec)
+            spec.resources = ResourceSpecV1(process_count=N_PROC)
+            ex = _get_executor_instance(execparams, job)
+            ex.submit(job)
+            status = job.wait()
+            assert_completed(job, status)
