9504745e151241e4f3a68a4cd37098a819e63802
hategan <mhategan@gmail.com>
2025-02-25 09:39:26 -0800

    It appears that mpirun from 5 adds a space after the `:`.

--- src/psij/launchers/scripts/mpi_launch.sh
+++ src/psij/launchers/scripts/mpi_launch.sh
@@ -23,10 +23,18 @@ filter_err() {
     sed -nE 's/^\[[^]]+\]<stderr>:(.*)/\1/p'
 }
 
+filter_out_5() {
+    sed -nE 's/^\[[^]]+\]<stdout>: (.*)/\1/p'
+}
+
+filter_err_5() {
+    sed -nE 's/^\[[^]]+\]<stderr>: (.*)/\1/p'
+}
+
 set +e
 if [ "$IS_OPENMPI_5" == "1" ]; then
     mpirun --oversubscribe --output TAG -n $_PSI_J_PROCESS_COUNT "$@" \
-        1> >(filter_out > $_PSI_J_STDOUT) 2> >(filter_err > $_PSI_J_STDERR) <$_PSI_J_STDIN
+        1> >(filter_out_5 > $_PSI_J_STDOUT) 2> >(filter_err_5 > $_PSI_J_STDERR) <$_PSI_J_STDIN
 elif [ "$IS_OPENMPI" == "1" ]; then
     mpirun --oversubscribe --tag-output -q -n $_PSI_J_PROCESS_COUNT "$@" \
         1> >(filter_out > "$_PSI_J_STDOUT") 2> >(filter_err > $_PSI_J_STDERR) <$_PSI_J_STDIN
